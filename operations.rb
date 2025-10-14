c9e9c44fae02a37dfe5c7c9fa86828866d6a9b52
Raphaël Bakhouche <raphael.bakhouche@epitech.eu>
2025-10-14 20:14:41 +0200

    init

---
+++ operations.rb
@@ -0,0 +1,11 @@
+puts "Priorités basiques"
+puts 1 + 2 * 3 + 4
+puts (1 + 2) * 3 + 4
+
+puts "\nInt + Int vs Int + Float"
+puts 1 + 1
+puts 1 + 1.0
+
+puts "\nInt / Int vs Int / Float"
+puts 3 / 2
+puts 3 / 2.0
